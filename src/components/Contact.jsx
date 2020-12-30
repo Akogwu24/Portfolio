80da050dca8b69a9f867860ac21967f208673b71
Akogwu Emma <akogwu.emmanuel24@gmial.com>
2020-12-30 17:43:57 +0100

    icons added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,30 @@
 import React from "react";
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Link } from "@chakra-ui/react";
 
 import { ReactComponent as IG } from "../images/instagram.svg";
-import { ReactComponent as Twitter } from "../images/facebook2.svg";
+import { ReactComponent as Twitter } from "../images/twitter.svg";
+import { ReactComponent as FB } from "../images/facebook1.svg";
+import { ReactComponent as Call } from "../images/call1.svg";
 
 function Contact() {
   return (
-    <Flex>
-      <IG />
-      <Twitter />
+    <Flex justify="center" align="center" mt="30px">
+      <Box h="2px" w="120%" bg="rgba(219, 211, 215, .1)"></Box>
+      <Link>
+        <Call as="a" />
+      </Link>
+
+      <Link>
+        <IG />
+      </Link>
+
+      <Link>
+        <Twitter />
+      </Link>
+      <Link>
+        <FB />
+      </Link>
+      <Box h="2px" w="120%" bg="rgba(219, 211, 215, .1)"></Box>
     </Flex>
   );
 }
